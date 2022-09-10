e3150cf1fb6b10329b293552faf92a2e877323c9
Izkil <oderinde.izkil@gmail.com>
2022-09-10 06:10:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #!/bin/bash
-include <studio.h>
+#include <stdio.h>
 /**
-* main- print the string in the put function
-
-* Description - using the main function
-* this program prints "Programming is like building a multi* lingual puzzle
-* Return : 0
-/
+ * * main - main block
+ * * Return: 0
+ * */
 int main(void)
 {
-	puts("\""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
