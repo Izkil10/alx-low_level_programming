1fb037a7354bdaa584700e55280c66b2a4e74378
Izkil <oderinde.izkil@gmail.com>
2022-09-28 05:57:02 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,11 +15,13 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (j = 0; accept[j]; j++)
 		{
-			if (s[i] == accept[j])
+			if
+			       	(s[i] == accept[j])
 
 				break;
 		}
-		if (!accept[j])
+		if
+		       	(!accept[j])
 
 
 				break;
